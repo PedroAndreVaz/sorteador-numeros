924742f0e45b6d21557559bbefd3db12248cffba
Pedro Vaz <87241517+PedroAndreVaz@users.noreply.github.com>
2025-04-20 18:38:58 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    if (quantidade > (ate - de + 1)) {
+        alert('erro no preenchimento dos campos. verifique');
+        return;
+    }
+    
+    
+    if (de >= ate) {
+        alert ('verifique se você preencheu os campos corretamente');
+        return;
+    }
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de,ate);
+        
+        while(sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio (de,ate);
+            alert('Tentando obter número inédito');
+        }
+        
+        sorteados.push(numero);
+    
+
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = `<label class="texto_paragrafo" >Números sorteados: ${sorteados}</label>`
+alterarStatusBotao();
+    
+
+    }
+    
+ }
+
+ function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+            botao.classList.remove('container__botao-desabilitado');
+            botao.classList.add('container__botao');
+    } else {
+            botao.classList.remove('container__botao');
+            botao.classList.add('container__botao-desabilitado');
+    }
+}   
+    
+
+function obterNumeroAleatorio (min,max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function reiniciar () {
+        document.getElementById('quantidade').value = '';
+        document.getElementById('de').value = '';
+        document.getElementById('ate').value = '';
+        document.getElementById('resultado').innerHTML = 'Números sorteados:  nenhum até agora'
+        alterarStatusBotao ();
+    }
